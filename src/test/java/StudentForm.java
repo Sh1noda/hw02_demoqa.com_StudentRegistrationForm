c8f6f5f9c51974f277a30aa42871e411a76c9d85
ilanovikov <novikov.redstar@yandex.ru>
2022-11-16 20:07:04 +0300

    added test

--- src/test/java/StudentForm.java
+++ src/test/java/StudentForm.java
@@ -1,9 +1,17 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class StudentForm {
     @Test
-    void someTest() {
-        System.out.println("Hello World!");
-
+    void fillStudentFormTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $("[id=firstName]").setValue("Пёс");
+        $("[id=lastName]").setValue("Шарик");
+        $("[id=userEmail]").setValue("DogsHeart@google.com");
     }
 }
+
+
