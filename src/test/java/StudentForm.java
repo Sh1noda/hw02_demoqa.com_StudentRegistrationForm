b1430f72e72268243cbbbc737f980fa82607f451
ilanovikov <novikov.redstar@yandex.ru>
2022-11-17 14:43:19 +0300

    final push

--- src/test/java/StudentForm.java
+++ src/test/java/StudentForm.java
@@ -19,26 +19,26 @@ public class StudentForm {
         open("https://demoqa.com/automation-practice-form");
 
         //Name
-        $("[id=firstName]").setValue("Пёс");
-        $("[id=lastName]").setValue("Шарик");
+        $("#firstName").setValue("Пёс");
+        $("#lastName").setValue("Шарик");
 
         //Email
-        $("[id=userEmail]").setValue("DogsHeart@google.com");
+        $("#userEmail").setValue("DogsHeart@google.com");
 
         //Gender
         $(byText("Other")).shouldBe(visible).click();
 
         //Mobile
-        $("[id=userNumber]").setValue("1234567890");
+        $("#userNumber").setValue("1234567890");
 
         //Date of Birth
-        $("[id=dateOfBirthInput]").click();
-        $("[class=react-datepicker__month-select]").selectOption("February");
-        $("[class=react-datepicker__year-select]").selectOption("1988");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1988");
         $x("//div[@aria-label='Choose Monday, February 29th, 1988']").click();
 
         //Subject
-        $("[id=subjectsInput]").setValue("Computer").pressTab();
+        $("#subjectsInput").setValue("Computer").pressTab();
 
         //Hobbies
         $(byText("Sports")).shouldBe(visible).click();
@@ -46,30 +46,30 @@ public class StudentForm {
         $(byText("Music")).shouldBe(visible).click();
 
         //Picture
-        $("[id=uploadPicture]").uploadFile(new File("src/test/resources/sharik.jpeg"));
+        $("#uploadPicture").uploadFile(new File("src/test/resources/sharik.jpeg"));
 
         //Current Address
-        $("[id=currentAddress]").setValue("Moscow, Prechistenka str, 24/1");
+        $("#currentAddress").setValue("Moscow, Prechistenka str, 24/1");
 
         //State and city
-        $("[id=react-select-3-input]").setValue("Haryana").pressTab();
-        $("[id=react-select-4-input]").setValue("Panipat").pressTab();
+        $("#react-select-3-input").setValue("Haryana").pressTab();
+        $("#react-select-4-input").setValue("Panipat").pressTab();
 
         //Submit button
-        $("[id=submit]").click();
+        $("#submit").click();
 
         //Final Check
-        $("[class=modal-content]").shouldHave(text("Thanks for submitting the form"));
-        $("[class=modal-content]").shouldHave(text("Student Name"), text("Пёс Шарик"));
-        $("[class=modal-content]").shouldHave(text("Student Email"), text("DogsHeart@google.com"));
-        $("[class=modal-content]").shouldHave(text("Gender"), text("Other"));
-        $("[class=modal-content]").shouldHave(text("Mobile"), text("1234567890"));
-        $("[class=modal-content]").shouldHave(text("Date of Birth"), text("29 February,1988"));
-        $("[class=modal-content]").shouldHave(text("Subjects"), text("Computer Science"));
-        $("[class=modal-content]").shouldHave(text("Hobbies"), text("Sports, Reading, Music"));
-        $("[class=modal-content]").shouldHave(text("Picture"), text("sharik.jpeg"));
-        $("[class=modal-content]").shouldHave(text("Address"), text("Moscow, Prechistenka str, 24/1"));
-        $("[class=modal-content]").shouldHave(text("State and City"), text("Haryana Panipat"));
+        $(".modal-content").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(text("Student Name"), text("Пёс Шарик"));
+        $(".modal-content").shouldHave(text("Student Email"), text("DogsHeart@google.com"));
+        $(".modal-content").shouldHave(text("Gender"), text("Other"));
+        $(".modal-content").shouldHave(text("Mobile"), text("1234567890"));
+        $(".modal-content").shouldHave(text("Date of Birth"), text("29 February,1988"));
+        $(".modal-content").shouldHave(text("Subjects"), text("Computer Science"));
+        $(".modal-content").shouldHave(text("Hobbies"), text("Sports, Reading, Music"));
+        $(".modal-content").shouldHave(text("Picture"), text("sharik.jpeg"));
+        $(".modal-content").shouldHave(text("Address"), text("Moscow, Prechistenka str, 24/1"));
+        $(".modal-content").shouldHave(text("State and City"), text("Haryana Panipat"));
     }
 }
 
